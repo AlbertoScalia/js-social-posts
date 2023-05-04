5b88e4d6f3cd401ead06c9d80cfa8ec4049f3d83
AlbertoScalia <albscalia@gmail.com>
2023-05-04 15:58:51 +0200

    Feat (JS) : Add card cycle

--- main.js
+++ main.js
@@ -55,3 +55,39 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+const postElements = document.querySelector('.posts-list');
+
+//Ciclo for per generare le card
+for (let i = 0; i < posts.length; i++) {
+    postElements.innerHTML += `<div class="post">
+<div class="post__header">
+    <div class="post-meta">                    
+        <div class="post-meta__icon">
+            <img class="profile-pic" src="${posts[i].author.image || 'https://via.placeholder.com/150x150.png?text=' + nullImage}" alt="">                    
+        </div> 
+         <div class="post-meta__data">
+         <div class="post-meta__author">${posts[i].author.name}</div>
+         <div class="post-meta__time">4 mesi fa</div>
+        </div>                    
+    </div>
+</div>
+<div class="post__text">${posts[i].content}</div>
+<div class="post__image">
+    <img src="${posts[i].media}" alt="">
+</div>
+<div class="post__footer">
+    <div class="likes js-likes">
+        <div class="likes__cta">
+        <a class="like-button  js-like-button" data-postid="${posts[i].id}">
+            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+            <span class="like-button__label">Mi Piace</span>
+        </a>
+    </div>
+        <div class="likes__counter">
+            Piace a <b id="like-counter-1" class="js-likes-counter">${posts[i].likes}</b> persone
+        </div>
+    </div> 
+</div>            
+</div>`
+}
